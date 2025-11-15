08127a864b862b9c6f2f7b04e4afe45b131a1eea
Muroslav11 <Muroslavhalik@gmail.com>
2025-11-15 22:09:09 +0200

    Lab6.1.1

---
+++ Lab6.1.1/Lab6.1.1/Lab6.1.1.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+// --- Генерація масиву ---
+void Create(int* c, const int size, const int Low, const int High)
+{
+    for (int i = 0; i < size; i++)
+        c[i] = Low + rand() % (High - Low + 1);
+}
+
+// --- Виведення масиву ---
+void Print(int* c, const int size)
+{
+    for (int i = 0; i < size; i++)
+        cout << setw(4) << c[i];
+    cout << endl;
+}
+
+// --- Перевірка критерію ---
+bool Criterion(int value, int index)
+{
+    return (value % 2 != 0) && (index % 13 != 0);
+}
+
+// --- Підрахунок кількості ---
+int countSelected(const int c[], int size)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+        if (Criterion(c[i], i))
+            count++;
+    return count;
+}
+
+// --- Сума відібраних ---
+int sumSelected(const int c[], int size)
+{
+    int sum = 0;
+    for (int i = 0; i < size; i++)
+        if (Criterion(c[i], i))
+            sum += c[i];
+    return sum;
+}
+
+// --- Заміна відібраних елементів нулями ---
+void replaceWithZero(int c[], int size)
+{
+    for (int i = 0; i < size; i++)
+        if (Criterion(c[i], i))
+            c[i];
+        else
+            c[i] = 0;
+}
+
+int main()
+{
+    srand((unsigned)time(nullptr));
+
+    const int SIZE = 21;
+    int c[SIZE];
+
+    // 1. Генерація
+    Create(c, SIZE, 15, 85);
+
+    // 2. Виведення початкового масиву
+    cout << "Initial array:" << endl;
+    Print(c, SIZE);
+
+    // 3. Обчислення
+    int count = countSelected(c, SIZE);
+    int sum = sumSelected(c, SIZE);
+
+    // 4. Модифікація
+    replaceWithZero(c, SIZE);
+
+    // 5. Результати
+    cout << "\nCount: " << count << endl;
+    cout << "Sum: " << sum << endl;
+    cout << "Modified array:" << endl;
+    Print(c, SIZE);
+
+    return 0;
+}
---
+++ Lab6.1.1/UnitTest6.1.1/UnitTest6.1.1.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab6.1.1/Lab6.1.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest611
+{
+	TEST_CLASS(UnitTest611)
+	{
+	public:
+		
+        TEST_METHOD(Test_Criterion)
+        {
+            // value непарне, index не кратний 13 → TRUE
+            Assert::IsTrue(Criterion(17, 5));
+
+            // value парне → FALSE
+            Assert::IsFalse(Criterion(20, 5));
+
+            // index кратний 13 → FALSE
+            Assert::IsFalse(Criterion(15, 13));
+
+            // value парне і index кратний 13 → FALSE
+            Assert::IsFalse(Criterion(84, 26));
+        }
+
+        TEST_METHOD(Test_CountSelected)
+        {
+            int c[5] = { 15, 17, 12, 19, 21 };
+
+            int result = countSelected(c, 5);
+            Assert::AreEqual(3, result);
+        }
+
+        TEST_METHOD(Test_SumSelected)
+        {
+            int c[5] = { 18, 20, 15, 21, 19 };
+            // сума = 15 + 21 + 19 = 55
+
+            int result = sumSelected(c, 5);
+            Assert::AreEqual(55, result);
+        }
+
+	};
+}

0fec8f1b02bd14d682d2f0b4553a4473df46b031
Muroslav11 <Muroslavhalik@gmail.com>
2025-11-17 23:04:48 +0200

    Lab6.3.2

---
+++ Lab6.3.2/Lab6.3.2/Lab6.3.2.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+
+// Рекурсивне створення масиву
+void Create(int* a, const int size, int Low, int High, int i)
+{
+    a[i] = Low + rand() % (High - Low + 1);
+    if (i < size - 1)
+        Create(a, size, Low, High, i + 1);
+}
+
+// Рекурсивний вивід масиву
+void Print(int* a, const int size, int i)
+{
+    cout << setw(4) << a[i];
+    if (i < size - 1)
+        Print(a, size, i + 1);
+    else
+        cout << endl;
+}
+
+// Рекурсивна функція пошуку MIN
+int Min(int* a, const int size, int i, int min)
+{
+    if (a[i] < min)
+        min = a[i];
+
+    if (i < size - 1)
+        return Min(a, size, i + 1, min);
+    else
+        return min;
+}
+
+// Рекурсивна шаблонна функція Min
+template <typename T>
+T Min(T* a, const int size, int i, T min)
+{
+    if (a[i] < min)
+        min = a[i];
+
+    if (i < size - 1)
+        return Min<T>(a, size, i + 1, min);
+    return min;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    const int n = 10;
+    int a[n];
+
+    int Low = -20;
+    int High = 20;
+
+    Create(a, n, Low, High, 0);
+
+    cout << "Array int:" << endl;
+    Print(a, n, 0);
+
+    // Звичайна функція
+    cout << "Min(int) = " << Min(a, n, 0, a[0]) << endl;
+
+    cout << "Min<int>(a, n) = " << Min<int>(a, n, 0, a[0]) << endl << endl;
+
+    // Приклад для double
+    double b[n] = { 2.5, 1.1, 9.3, -2.2, 5.5, 4.1, 0.0, -3.4, 6.6, 1.0 };
+    cout << "Min<double>(b, n) = " << Min<double>(b, n, 0, b[0]) << endl << endl;
+
+    // --- Масив string ---
+    string c[n] = { "5", "7", "1", "9", "3", "0", "2", "8", "6", "4" };
+    cout << "Min<string>(c, n) = " << Min<string>(c, n, 0, c[0]) << endl;
+
+    return 0;
+}
---
+++ Lab6.3.2/UnitTest6.3.2/UnitTest6.3.2.cpp
@@ -0,0 +1,42 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab6.3.2/Lab6.3.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest632
+{
+	TEST_CLASS(UnitTest632)
+	{
+	public:
+		
+        TEST_METHOD(TestMinIntRecursive)
+        {
+            int a[5] = { 4, -2, 7, 1, 0 };
+            int result = Min(a, 5, 0, a[0]);
+            Assert::AreEqual(-2, result);
+        }
+
+        TEST_METHOD(TestMinTemplateInt)
+        {
+            int a[5] = { 10, 3, -5, 7, 9 };
+            int result = Min<int>(a, 5, 0, a[0]);
+            Assert::AreEqual(-5, result);
+        }
+
+        TEST_METHOD(TestMinTemplateDouble)
+        {
+            double a[5] = { 2.5, -1.3, 9.0, 0.0, 1.1 };
+            double result = Min<double>(a, 5, 0, a[0]);
+            Assert::AreEqual(-1.3, result, 0.0001);
+        }
+
+        TEST_METHOD(TestMinTemplateString)
+        {
+            // Порівняння за алфавітом → "1" найменша
+            std::string a[5] = { "5", "1", "7", "3", "9" };
+            std::string result = Min<std::string>(a, 5, 0, a[0]);
+            Assert::AreEqual(std::string("1"), result);
+        }
+	};
+}

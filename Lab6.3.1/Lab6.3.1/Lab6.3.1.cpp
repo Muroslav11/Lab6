7664a33aa0e71afdf50772588ca60544d3dd6f86
Muroslav11 <Muroslavhalik@gmail.com>
2025-11-17 22:55:26 +0200

    Lab6.3.1

---
+++ Lab6.3.1/Lab6.3.1/Lab6.3.1.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <string>
+#include <time.h>
+using namespace std;
+
+// --- Формування масиву ---
+void Create(int* a, const int size, const int Low, const int High)
+{
+    for (int i = 0; i < size; i++)
+        a[i] = Low + rand() % (High - Low + 1);
+}
+
+// --- Форматоване виведення ---
+void Print(const int* a, int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << setw(4) << a[i];
+    cout << endl;
+}
+
+// --- Функція пошуку мінімального елемента (звичайна) ---
+int Min(int* a, const int size)
+{
+    int min = a[0];
+    for (int i = 1; i < size; i++)
+        if (a[i] < min)
+            min = a[i];
+    return min;
+}
+
+// --- Універсальна шаблонна функція ---
+template <typename T>
+T Min(T* a, const int size)
+{
+    T min = a[0];
+    for (int i = 1; i < size; i++)
+        if (a[i] < min)
+            min = a[i];
+    return min;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    const int n = 10;
+
+    // --- Масив int ---
+    int a[n];
+    int Low = -20;
+    int High = 20;
+    Create(a, n, Low, High);
+    cout << "Array int:" << endl;
+    Print(a, n);
+
+    cout << "Min(int) = " << Min(a, n) << endl;
+    cout << "Min<int>(a, n) = " << Min<int>(a, n) << endl << endl;
+
+    // --- Масив double ---
+    double b[n] = { 2.5, 1.1, 9.3, -2.2, 5.5, 4.1, 0.0, -3.4, 6.6, 1.0 };
+    cout << "Min<double>(b, n) = " << Min<double>(b, n) << endl << endl;
+
+    // --- Масив string ---
+    string c[n] = { "5", "7", "1", "9", "3", "0", "2", "8", "6", "4" };
+    cout << "Min<string>(c, n) = " << Min<string>(c, n) << endl;
+
+    return 0;
+}
---
+++ Lab6.3.1/UnitTest6.3.1/UnitTest6.3.1.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab6.3.1/Lab6.3.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest631
+{
+	TEST_CLASS(UnitTest631)
+	{
+	public:
+		
+		TEST_METHOD(Test_Min_Int_Normal)
+		{
+			int a[5] = { 4, -2, 7, 1, 0 };
+
+			Assert::AreEqual(-2, Min(a, 5));          // звичайна
+			Assert::AreEqual(-2, Min<int>(a, 5));     // шаблон
+		}
+
+		TEST_METHOD(Test_Min_Double)
+		{
+			double a[6] = { 2.5, -1.1, 9.3, 0.0, -3.4, 5.5 };
+
+			double result = Min<double>(a, 6);
+
+			Assert::AreEqual(-3.4, result, 0.0001);
+		}
+
+		TEST_METHOD(Test_Min_String)
+		{
+			string s[5] = { "5", "2", "9", "1", "7" };
+
+			string result = Min<string>(s, 5);
+
+			Assert::AreEqual(string("1"), result);
+		}
+	};
+}
